2907a1c5c10df9583059c606a9df84e5b079ff66
h-nriquevieira <h.nriquevieira@gmail.com>
2023-08-26 13:35:38 -0300

    feat: add login functionality

---
+++ src/service/AuthServices.ts
@@ -0,0 +1,25 @@
+import { supabase } from "../config/supabase";
+
+export async function signUpWithEmail(email: string, password: string) {
+  const res = await supabase.auth.signUp({ email, password });
+  return res;
+}
+
+export async function signInWithEmail(email: string, password: string) {
+  const res = await supabase.auth.signInWithPassword({ email, password });
+  return res;
+}
+
+export async function signOut() {
+  return await supabase.auth.signOut();
+}
+
+export async function signInWithGoogle() {
+  return await supabase.auth.signInWithOAuth({
+    provider: "google",
+  });
+}
+
+export async function getUserInfo() {
+  return await supabase.auth.getUser();
+}
