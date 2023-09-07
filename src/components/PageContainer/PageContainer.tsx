197bcc01b138cd5b4ee903af627d74b5d49d6fd6
h-nriquevieira <h.nriquevieira@gmail.com>
2023-09-07 15:58:35 -0300

    fix: PageContainer styles

--- src/components/PageContainer/PageContainer.tsx
+++ src/components/PageContainer/PageContainer.tsx
@@ -7,16 +7,19 @@ export default function PageContainer({ children }: PropsWithChildren) {
     query: "(max-width: 800px)",
   });
 
-  return (
-    <Box
-      style={{
-        padding: isSmallScreen ? "2rem 2rem" : "2rem 4rem",
-        width: "100%",
-        maxHeight: "100vh",
-        overflow: "scroll",
-      }}
-    >
-      {children}
-    </Box>
-  );
+  const generalStyles = {
+    padding: isSmallScreen ? "2rem 2rem" : "2rem 4rem",
+    width: "100%",
+  };
+
+  const desktopStyles = {
+    maxHeight: "100vh",
+    overflow: "scroll",
+  };
+
+  const styles = isSmallScreen
+    ? generalStyles
+    : { ...generalStyles, ...desktopStyles };
+
+  return <Box style={styles}>{children}</Box>;
 }
