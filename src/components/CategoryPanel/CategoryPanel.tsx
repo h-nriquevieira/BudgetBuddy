9aa92aafae708ad78c7de32c797e4644a7b51e4f
h-nriquevieira <h.nriquevieira@gmail.com>
2023-09-08 17:59:38 -0300

    feat: finish NewCategoryPopover layout

--- src/components/CategoryPanel/CategoryPanel.tsx
+++ src/components/CategoryPanel/CategoryPanel.tsx
@@ -1,9 +1,10 @@
-import { Box, Button, Callout, Heading } from "@radix-ui/themes";
+import { Box, Button, Callout, Popover, Heading } from "@radix-ui/themes";
 import * as Accordion from "@radix-ui/react-accordion";
 import CategoryAccordionItem from "../CategoryAccordionItem/CategoryAccordionItem";
 import styles from "./styles.module.css";
 import { CategoryResponse } from "../../types/CategoryTypes";
 import { InfoCircledIcon, PlusIcon } from "@radix-ui/react-icons";
+import NewCategoryPopover from "../NewCategoryPopover/NewCategoryPopover";
 
 type CategoryPanelProps = {
   categories: CategoryResponse[];
@@ -15,34 +16,48 @@ export default function CategoryPanel({ categories }: CategoryPanelProps) {
   };
 
   return (
-    <Box style={{ display: "flex", flexDirection: "column" }}>
-      <Heading as="h2" color="jade" style={headingStyles}>
-        Categorias
-      </Heading>
-      {!categories.length && (
-        <Callout.Root style={{ marginTop: "2rem" }} color="amber">
-          <Callout.Icon>
-            <InfoCircledIcon />
-          </Callout.Icon>
-          <Callout.Text>Adicione sua primeira categoria.</Callout.Text>
-        </Callout.Root>
-      )}
-      <Button
-        style={{ marginTop: "2rem", marginLeft: "auto", cursor: "pointer" }}
-      >
-        <PlusIcon />
-        Nova categoria
-      </Button>
-      <Accordion.Root
-        type="single"
-        collapsible
-        className={styles.AccordionRoot}
-      >
-        {categories &&
-          categories.map((category) => (
-            <CategoryAccordionItem key={category.id} category={category} />
-          ))}
-      </Accordion.Root>
-    </Box>
+    <>
+      <Box style={{ display: "flex", flexDirection: "column" }}>
+        <Heading as="h2" color="jade" style={headingStyles}>
+          Categorias
+        </Heading>
+        {!categories.length && (
+          <Callout.Root style={{ marginTop: "2rem" }} color="amber">
+            <Callout.Icon>
+              <InfoCircledIcon />
+            </Callout.Icon>
+            <Callout.Text>Adicione sua primeira categoria.</Callout.Text>
+          </Callout.Root>
+        )}
+        <Popover.Root>
+          <Popover.Trigger>
+            <Button
+              style={{
+                marginTop: "2rem",
+                marginLeft: "auto",
+                cursor: "pointer",
+              }}
+            >
+              <PlusIcon />
+              Nova categoria
+            </Button>
+          </Popover.Trigger>
+
+          <Popover.Content>
+            <NewCategoryPopover />
+          </Popover.Content>
+        </Popover.Root>
+        <Accordion.Root
+          type="single"
+          collapsible
+          className={styles.AccordionRoot}
+        >
+          {categories &&
+            categories.map((category) => (
+              <CategoryAccordionItem key={category.id} category={category} />
+            ))}
+        </Accordion.Root>
+      </Box>
+    </>
   );
 }
